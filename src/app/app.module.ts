3e9855c6a1a5ea25357d830769a1f54a8f50dd51
Arnaud <abracchetti@gmail.com>
2017-11-03 14:12:15 +0100

    add fadein fadeout to the spaceDefinitionForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
@@ -30,6 +31,7 @@ import { FormsModule } from '@angular/forms';
     imports: [
         BrowserModule,
         FormsModule,
+        BrowserAnimationsModule,
         AngularFireModule.initializeApp(environment.firebase),
         AngularFireDatabaseModule,
         RouterModule.forRoot(
--- src/app/board-definition/space-definition-form/space-definition-form.component.ts
+++ src/app/board-definition/space-definition-form/space-definition-form.component.ts
@@ -1,14 +1,29 @@
 import { SpaceDefinition } from '../model/space-definition';
+import { style, trigger, state, transition, animate } from '@angular/animations';
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'fd-space-definition-form',
     templateUrl: './space-definition-form.component.html',
-    styleUrls: ['./space-definition-form.component.scss']
+    styleUrls: ['./space-definition-form.component.scss'],
+    animations: [
+        trigger('visible', [
+
+            transition(':enter', [
+                style({ opacity: '0' }),
+                animate(200)]),
+            transition(':leave', [
+                animate(200,
+                    style({ opacity: '0' }))])
+        ])
+
+
+    ]
 })
 export class SpaceDefinitionFormComponent implements OnInit {
 
     @Input() model: SpaceDefinition;
+    @Input() visible: boolean;
 
     constructor() { }
 
